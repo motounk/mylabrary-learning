935dc6e707acecdfcc83eea568841384ade62db9
Mohamed Aliou Tounkara <momotounk@yahoo.fr>
2020-06-30 15:13:10 +0200

    Finished Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 // ALl Authors Route
 router.get('/', async (req, res) => {
@@ -29,13 +30,72 @@ router.post('/', async (req, res) => {
 	});
 	try {
 		const newAuthor = await author.save();
-		//res.redirect(`authors/${newAuthor.id}`)
-		res.redirect(`authors`);
+		res.redirect(`authors/${newAuthor.id}`);
 	} catch (error) {
 		res.render('authors/new', {
 			author: author,
 			errorMessage: 'Error creating Author'
 		});
+	}
+});
+
+//Show author
+router.get('/:id', async (req, res) => {
+	try {
+		const author = await Author.findById(req.params.id);
+		const books = await Book.find({ author: author.id }).limit(6).exec();
+		res.render('authors/show', {
+			author: author,
+			booksByAuthor: books
+		});
+	} catch (error) {
+		res.redirect('/');
+	}
+});
+
+// Edit author form
+router.get('/:id/edit', async (req, res) => {
+	try {
+		const author = await Author.findById(req.params.id);
+		res.render('authors/edit', { author: author });
+	} catch (error) {
+		res.redirect('/authors');
+	}
+});
+
+// Update author route
+router.put('/:id', async (req, res) => {
+	let author;
+	try {
+		author = await Author.findById(req.params.id);
+		author.name = req.body.name;
+		await author.save();
+		res.redirect(`/authors/${author.id}`);
+	} catch (error) {
+		if (author == null) {
+			res.redirect('/');
+		} else {
+			res.render('authors/new', {
+				author: author,
+				errorMessage: 'Error updating Author'
+			});
+		}
+	}
+});
+
+// Delete author
+router.delete('/:id', async (req, res) => {
+	let author;
+	try {
+		author = await Author.findById(req.params.id);
+		await author.remove();
+		res.redirect('/authors');
+	} catch (error) {
+		if (author == null) {
+			res.redirect('/');
+		} else {
+			res.redirect(`/authors/${author.id}`);
+		}
 	}
 });
 
