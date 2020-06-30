f248bc402eb10bef705195f7defd685266bc6a69
Mohamed Aliou Tounkara <momotounk@yahoo.fr>
2020-06-30 13:47:50 +0200

    File Upload Database

--- routes/books.js
+++ routes/books.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 const Book = require('../models/book');
 const Author = require('../models/author');
-const uploadPath = path.join('public', Book.coverImageBasePath);
 const imageMimeTypes = [ 'image/jpeg', 'image/png', 'images/gif' ];
-const upload = multer({
-	dest: uploadPath,
-	fileFilter: (req, file, callback) => {
-		callback(null, imageMimeTypes.includes(file.mimetype));
-	}
-});
 
 // All Books Route
 router.get('/', async (req, res) => {
-	res.send('All books');
 	let query = Book.find();
 	if (req.query.title != null && req.query.title != '') {
 		query = query.regex('title', new RegExp(req.query.title, 'i'));
@@ -44,34 +33,24 @@ router.get('/new', async (req, res) => {
 });
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
-	const fileName = req.file != null ? req.file.filename : null;
+router.post('/', async (req, res) => {
 	const book = new Book({
 		title: req.body.title,
 		author: req.body.author,
 		publishDate: new Date(req.body.publishDate),
 		pageCount: req.body.pageCount,
-		coverImageName: fileName,
 		description: req.body.description
 	});
+	saveCover(book, req.body.cover);
 
 	try {
 		const newBook = await book.save();
 		// res.redirect(`books/${newBook.id}`)
 		res.redirect(`books`);
 	} catch (error) {
-		if (book.coverImageName != null) {
-			removeBookCover(book.coverImageName);
-		}
 		renderNewPage(res, book, true);
 	}
 });
-
-function removeBookCover(fileName) {
-	fs.unlink(path.join(uploadPath, fileName), (err) => {
-		if (err) console.error(err);
-	});
-}
 
 async function renderNewPage(res, book, hasError = false) {
 	try {
@@ -84,6 +63,15 @@ async function renderNewPage(res, book, hasError = false) {
 		res.render('books/new', params);
 	} catch (error) {
 		res.redirect('/books');
+	}
+}
+
+function saveCover(book, coverEncoded) {
+	if (coverEncoded == null) return;
+	const cover = JSON.parse(coverEncoded);
+	if (cover != null && imageMimeTypes.includes(cover.type)) {
+		book.coverImage = new Buffer.from(cover.data, 'base64');
+		book.coverImageType = cover.type;
 	}
 }
 
