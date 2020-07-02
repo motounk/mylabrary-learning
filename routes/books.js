b9f7e637e2e4159031d939d27666630f59d10065
Mohamed Aliou Tounkara <momotounk@yahoo.fr>
2020-07-02 18:51:03 +0200

    Finish book routes

--- routes/books.js
+++ routes/books.js
@@ -45,22 +45,98 @@ router.post('/', async (req, res) => {
 
 	try {
 		const newBook = await book.save();
-		// res.redirect(`books/${newBook.id}`)
-		res.redirect(`books`);
+		res.redirect(`books/${newBook.id}`);
 	} catch (error) {
 		renderNewPage(res, book, true);
 	}
 });
 
+// Show Book Route
+router.get('/:id', async (req, res) => {
+	try {
+		const book = await Book.findById(req.params.id).populate('author').exec();
+		res.render('books/show', { book: book });
+	} catch (e) {
+		res.redirect('/');
+	}
+});
+
+// Edit form for book
+router.get('/:id/edit', async (req, res) => {
+	try {
+		const book = await Book.findById(req.params.id);
+		renderEditPage(res, book);
+	} catch (error) {
+		res.redirect('/');
+	}
+});
+
+// Update book
+router.put('/:id', async (req, res) => {
+	let book;
+	try {
+		book = await Book.findById(req.params.id);
+		book.title = req.body.title;
+		book.author = req.body.author;
+		book.publishDate = new Date(req.body.publishDate);
+		book.pageCount = req.body.pageCount;
+		book.description = req.body.description;
+		if (req.body.cover != null && req.body.cover !== '') {
+			saveCover('books', req.body.cover !== '');
+		}
+		await book.save();
+		res.redirect(`/books/${book.id}`);
+	} catch (error) {
+		if (book != null) {
+			renderEditPage(res, book, true);
+		} else {
+			redirect('/');
+		}
+	}
+});
+
+// Delete book
+router.delete('/:id', async (req, res) => {
+	let book;
+	try {
+		book = await Book.findById(req.params.id);
+		await book.remove();
+		res.redirect('/books');
+	} catch (error) {
+		if (book != null) {
+			res.render('books/show', {
+				book: book,
+				errorMessage: 'Could not remove book'
+			});
+		} else {
+			res.redirect('/');
+		}
+	}
+});
+
 async function renderNewPage(res, book, hasError = false) {
+	renderFormPage(res, book, 'new', hasError);
+}
+
+async function renderEditPage(res, book, hasError = false) {
+	renderFormPage(res, book, 'edit', hasError);
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
 	try {
 		const authors = await Author.find({});
 		const params = {
 			authors: authors,
 			book: book
 		};
-		if (hasError) params.errorMessage = 'Error Creating Book';
-		res.render('books/new', params);
+		if (hasError) {
+			if (form === 'edit') {
+				params.errorMessage = 'Error Updating Book';
+			} else {
+				params.errorMessage = 'Error Creating Book';
+			}
+		}
+		res.render(`books/${form}`, params);
 	} catch (error) {
 		res.redirect('/books');
 	}
